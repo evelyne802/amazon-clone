f4cd505cc288fb7ba317704e14a741c8536384fa
evelyne802 <linagluzman@gmail.com>
2024-04-09 00:11:51 +0300

    feat: sign in button + new popup components

---
+++ src/app/components/location-popup/location-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './location-popup.component.html',
+  styleUrl: './location-popup.component.css'
+})
+export class LocationPopupComponent {
+
+}
---
+++ src/app/components/sign-in-popup/sign-in-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-in-popup.component.html',
+  styleUrl: './sign-in-popup.component.css'
+})
+export class SignInPopupComponent {
+
+}
