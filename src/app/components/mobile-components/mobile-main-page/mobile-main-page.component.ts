68f6dd77f6df81df70cfefe5571baf07b101a472
evelyne802 <linagluzman@gmail.com>
2024-05-07 20:59:54 +0300

    feat: image carousel (mobile)

--- src/app/components/mobile-components/mobile-main-page/mobile-main-page.component.ts
+++ src/app/components/mobile-components/mobile-main-page/mobile-main-page.component.ts
@@ -14,4 +14,51 @@ import { MobileFooterComponent } from '../mobile-footer/mobile-footer.component'
 })
 export class MobileMainPageComponent {
 
+  ngOnInit(){
+    this.mainCarousel();
+  }
+
+
+  mainCarousel(){
+    const carousel : any = document.querySelector('.carousel')
+    const slider = carousel?.querySelector('.carousel_track')
+    let slides = [...slider?.children]
+
+    // Initial slides position, so user can go from first to last slide (click to the left first)
+    slider?.prepend(slides[slides.length - 1])
+
+    // Handling arrow buttons
+    const handleArrowClick = (arrow: any) => {
+      arrow.addEventListener('click', () => {
+        slides = [...slider.children]
+        const currSlide = slider.querySelector('.is-selected')
+        currSlide.classList.remove('is-selected')
+        let targetSlide
+
+        if (arrow.classList.contains('jsPrev')) {
+          targetSlide = currSlide.previousElementSibling
+          slider.prepend(slides[slides.length - 1])
+        }
+
+        if (arrow.classList.contains('jsNext')) {
+          targetSlide = currSlide.nextElementSibling
+          slider.append(slides[0])
+        }
+
+        targetSlide.classList.add('is-selected')
+      })
+    }
+
+    const buttons = carousel.querySelectorAll('.carousel_btn')
+    buttons.forEach(handleArrowClick)
+
+    const nextBtn = buttons[1]
+
+    // Auto sliding
+    const slideTiming = 5000;
+    let interval:any;
+    const slideInterval = () => interval = setInterval(() => nextBtn.click(), slideTiming)
+    slideInterval()
+  }
+
 }
